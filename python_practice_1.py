908a399d57b7de912120d64fd58d92a2f1e3f3ad
UmamaAbid <136912754+UmamaAbid@users.noreply.github.com>
2023-10-11 21:19:25 +0500

    Update python_practice_1.py

--- python_practice_1.py
+++ python_practice_1.py
@@ -12,3 +12,17 @@ def checkLeapYear(year):
 year = int(input("Enter Year to check weather it is a leap year or not:"))
 
 checkLeapYear(year)
+
+
+# CODE 
+
+sentence = "this is a python program"
+word = sentence.split()
+print(word)
+
+# We have to split this in ordered form like a is program python this
+
+sorted_words = sorted(word)
+for word in sorted_words:
+
+    print(word)
