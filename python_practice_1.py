0c3285b772562f5c218f0c545abc7cf5d8bfd2e5
UmamaAbid <136912754+UmamaAbid@users.noreply.github.com>
2023-10-11 20:33:44 +0500

    Initial commit

---
+++ python_practice_1.py
@@ -0,0 +1,14 @@
+# To check weather the year is a leap year or not
+
+# Making a function and passing the parameter year into it.
+
+def checkLeapYear(year):
+    if year % 400 == 0 or year % 100 != 0 and year % 4 == 0:
+        print("This year is a leap year")
+    else:
+        print("This is not a leap year")
+
+
+year = int(input("Enter Year to check weather it is a leap year or not."))
+
+checkLeapYear(year)
