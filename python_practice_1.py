05210ea1e1f032d9b054a15ddcef88f7f094e6d4
Umama <umamaabidchohan@gmail.com>
2023-10-11 22:17:15 +0500

    Add all the changes

--- python_practice_1.py
+++ python_practice_1.py
@@ -12,3 +12,35 @@ def checkLeapYear(year):
 year = int(input("Enter Year to check weather it is a leap year or not:"))
 
 checkLeapYear(year)
+
+# CODE 
+
+sentence = "this is a python program"
+word = sentence.split()
+print(word)
+
+# We have to split this in ordered form like a is program python this
+
+sorted_words = sorted(word)
+for word in sorted_words:
+
+    print(word)
+
+
+# CODE 3
+
+# To check weather the string is float or not
+
+x = input("Enter a word or any number: \n")
+
+def check_float_Number(x):
+    try:
+        float(x)
+        return True
+    except:
+        return False
+
+
+print(check_float_Number(x))
+
+
